e5572d7b16146da1bd7bf2b94526cdfa919d89b1
oddbear <oddbjorn.bakke@gmail.com>
2022-01-05 20:41:01 +0100

    Working application, but no "service logic" or unmute or volume.

---
+++ GoXLR.Force.Defaults/NotificationClient.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using AudioDeviceCmdlets;
+using CoreAudioApi;
+
+namespace GoXLR.Force.Defaults
+{
+    public class NotificationClient : IMMNotificationClient
+    {
+        private readonly Dictionary<string, MMDevice> _devices = new Dictionary<string, MMDevice>();
+        private readonly MMDeviceEnumerator _deviceEnumerator;
+        private readonly PolicyConfigClient _configClient;
+        
+        public NotificationClient()
+        {
+            _configClient = new PolicyConfigClient();
+
+            _deviceEnumerator = new MMDeviceEnumerator();
+            _deviceEnumerator.RegisterEndpointNotificationCallback(this);
+
+            var defaultRenderMultimedia = _deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eMultimedia);
+            var defaultRenderCommunication = _deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eRender, ERole.eCommunications);
+            var defaultCaptureMultimedia = _deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eCapture, ERole.eMultimedia);
+            var defaultCaptureCommunication = _deviceEnumerator.GetDefaultAudioEndpoint(EDataFlow.eCapture, ERole.eCommunications);
+
+            var devices = _deviceEnumerator.EnumerateAudioEndPoints(EDataFlow.eAll, EDeviceState.DEVICE_STATEMASK_ALL);
+            var count = devices.Count;
+
+            for (int i = 0; i < count; i++)
+            {
+                var mmDevice = devices[i];
+                _devices[mmDevice.ID] = mmDevice;
+            }
+
+            //Set initial defaults:
+            OnDefaultDeviceChanged(EDataFlow.eRender, ERole.eMultimedia, defaultRenderMultimedia.ID);
+            OnDefaultDeviceChanged(EDataFlow.eRender, ERole.eCommunications, defaultRenderCommunication.ID);
+            OnDefaultDeviceChanged(EDataFlow.eCapture, ERole.eMultimedia, defaultCaptureMultimedia.ID);
+            OnDefaultDeviceChanged(EDataFlow.eCapture, ERole.eCommunications, defaultCaptureCommunication.ID);
+        }
+
+        public int OnDeviceStateChanged(string pwstrDeviceId, int dwNewState)
+        {
+            //Does nothing?
+            Console.WriteLine($"OnDeviceStateChanged: {pwstrDeviceId}, {dwNewState}");
+            return 0;
+        }
+
+        public int OnDeviceAdded(string pwstrDeviceId)
+        {
+            //TODO: Implement:
+            Console.WriteLine($"OnDeviceAdded: {pwstrDeviceId}");
+            return 0;
+        }
+
+        public int OnDeviceRemoved(string pwstrDeviceId)
+        {
+            //TODO: Implement:
+            Console.WriteLine($"OnDeviceRemoved: {pwstrDeviceId}");
+            return 0;
+        }
+
+        private void SetAudioDevice(EDataFlow flow, ERole role, string pwstrDefaultDeviceId, string searchString)
+        {
+            if (string.IsNullOrWhiteSpace(pwstrDefaultDeviceId))
+                return;
+
+            pwstrDefaultDeviceId = pwstrDefaultDeviceId.Replace("\0", string.Empty);
+
+            var mmDevice = _devices
+                .Values
+                .Where(device => device.State == EDeviceState.DEVICE_STATE_ACTIVE)
+                .Where(device => device.DataFlow == flow)
+                .Where(device => device.FriendlyName.StartsWith(searchString))
+                .SingleOrDefault();
+
+            if (mmDevice == null)
+                return;
+
+            if (pwstrDefaultDeviceId == mmDevice.ID)
+                return;
+
+            _configClient.SetDefaultEndpoint(mmDevice.ID, role);
+        }
+
+        public int OnDefaultDeviceChanged(EDataFlow flow, ERole role, string pwstrDefaultDeviceId)
+        {
+            if (flow == EDataFlow.eRender && role == ERole.eMultimedia)
+            {
+                SetAudioDevice(flow, role, pwstrDefaultDeviceId, "System");
+                return 0;
+            }
+            
+            if (flow == EDataFlow.eRender && role == ERole.eCommunications)
+            {
+                SetAudioDevice(flow, role, pwstrDefaultDeviceId, "Chat");
+                return 0;
+            }
+
+            if (flow == EDataFlow.eCapture && role == ERole.eMultimedia)
+            {
+                SetAudioDevice(flow, role, pwstrDefaultDeviceId, "Chat Mic");
+                return 0;
+            }
+
+            if (flow == EDataFlow.eCapture && role == ERole.eCommunications)
+            {
+                SetAudioDevice(flow, role, pwstrDefaultDeviceId, "Chat Mic");
+                return 0;
+            }
+
+            Console.WriteLine($"OnDefaultDeviceChanged: {flow}, {role}, {pwstrDefaultDeviceId}");
+            return 0;
+        }
+
+        public int OnPropertyValueChanged(string pwstrDeviceId, ref PropertyKey key)
+        {
+            //Does nothing?
+            Console.WriteLine($"OnPropertyValueChanged: {pwstrDeviceId}, {key.fmtid} ... {key.pid}");
+            return 0;
+        }
+    }
+}
--- GoXLR.Force.Defaults/WorkerService.cs
+++ GoXLR.Force.Defaults/WorkerService.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.ServiceProcess;
+﻿using System.ServiceProcess;
 using System.Threading;
-using AudioDeviceCmdlets;
-using CoreAudioApi;
 
 namespace GoXLR.Force.Defaults
 {
     public class WorkerService : ServiceBase
     {
-        private readonly Thread _workerThread = null;
-        private readonly ManualResetEvent _manualResetEvent = new ManualResetEvent(false);
+        private readonly NotificationClient _notificationClient;
 
         public WorkerService()
         {
@@ -20,126 +16,9 @@ namespace GoXLR.Force.Defaults
             base.CanPauseAndContinue = true;
             base.AutoLog = true;
 
-            _workerThread = new Thread(WorkerMethod)
-            {
-                Name = $"{name} Worker Thread",
-                IsBackground = true
-            };
-            _workerThread.Start();
+            _notificationClient = new NotificationClient();
         }
-
-        public class NotificationClient : IMMNotificationClient
-        {
-            public int OnDeviceStateChanged(string pwstrDeviceId, int dwNewState)
-            {
-                Console.WriteLine($"OnDeviceStateChanged: {pwstrDeviceId}, {dwNewState}");
-                return 0;
-            }
-
-            public int OnDeviceAdded(string pwstrDeviceId)
-            {
-                Console.WriteLine($"OnDeviceAdded: {pwstrDeviceId}");
-                return 0;
-            }
-
-            public int OnDeviceRemoved(string pwstrDeviceId)
-            {
-                Console.WriteLine($"OnDeviceRemoved: {pwstrDeviceId}");
-                return 0;
-            }
-
-            public int OnDefaultDeviceChanged(EDataFlow flow, ERole role, string pwstrDefaultDeviceId)
-            {
-                Console.WriteLine($"OnDefaultDeviceChanged: {flow}, {role}, {pwstrDefaultDeviceId}");
-                return 0;
-            }
-
-            public int OnPropertyValueChanged(string pwstrDeviceId, ref PropertyKey key)
-            {
-                Console.WriteLine($"OnPropertyValueChanged: {pwstrDeviceId}, {key.fmtid} ... {key.pid}");
-                return 0;
-            }
-        }
-
-        private void WorkerMethod()
-        {
-            var enumerator = new MMDeviceEnumerator();
-
-            var client = new NotificationClient();
-            enumerator.RegisterEndpointNotificationCallback(client);
-
-            while (true)
-            {
-                try
-                {
-                    _manualResetEvent.WaitOne();
-
-                    //var audioDevices = enumerator.EnumerateAudioEndPoints(EDataFlow.eAll, EDeviceState.DEVICE_STATE_ACTIVE);
-
-                    //for (int i = 0; i < audioDevices.Count; i++)
-                    //{
-                    //    var device = audioDevices[i];
-                    //    if (device.FriendlyName.IndexOf("TC-Helicon GoXLR", StringComparison.OrdinalIgnoreCase) < 0)
-                    //        continue;
-                        
-                    //    //Un-mute if muted:
-                    //    if (device.AudioEndpointVolume.Mute)
-                    //    {
-                    //        device.AudioEndpointVolume.Mute = false;
-                    //    }
-
-                    //    //Set Volume to 100% if lower:
-                    //    if (device.AudioEndpointVolume.MasterVolumeLevelScalar < 1)
-                    //    {
-                    //        device.AudioEndpointVolume.MasterVolumeLevelScalar = 1;
-                    //    }
-
-                    //    if (device.FriendlyName.StartsWith("Chat Mic"))
-                    //    {
-                    //        //if (!device.Default)
-                    //        {
-                    //            //AudioDeviceHelper.SetDefaultDeviceForRole(audioDevice, ERole.eMultimedia);
-                    //        }
-
-                    //        //if (!device.DefaultCommunication)
-                    //        {
-                    //            //AudioDeviceHelper.SetDefaultDeviceForRole(audioDevice, ERole.eCommunications);
-                    //        }
-                    //    }
-
-                    //    if (device.FriendlyName.StartsWith("System"))
-                    //    {
-                    //        //if (!audioDevice.Default)
-                    //        {
-                    //            //AudioDeviceHelper.SetDefaultDeviceForRole(audioDevice, ERole.eMultimedia);
-                    //        }
-                    //    }
-
-                    //    if (device.FriendlyName.StartsWith("Chat"))
-                    //    {
-                    //        //if (!audioDevice.DefaultCommunication)
-                    //        {
-                    //            //AudioDeviceHelper.SetDefaultDeviceForRole(audioDevice, ERole.eCommunications);
-                    //        }
-                    //    }
-                    //}
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-
-                try
-                {
-                    Thread.Sleep(5);
-                }
-                catch (ThreadInterruptedException)
-                {
-                    //Thread was interupted (service stoped).
-                }
-            }
-        }
-
+        
 #if DEBUG
         public void Start()
         {
@@ -149,51 +28,22 @@ namespace GoXLR.Force.Defaults
 
         protected override void OnStart(string[] args)
         {
-            try
-            {
-                _manualResetEvent.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            //TODO: Re-implement
         }
 
         protected override void OnContinue()
         {
-            try
-            {
-                _manualResetEvent.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            //TODO: Re-implement
         }
 
         protected override void OnPause()
         {
-            try
-            {
-                _manualResetEvent.Reset();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            //TODO: Re-implement
         }
 
         protected override void OnStop()
         {
-            try
-            {
-                _manualResetEvent.Reset();
-                _workerThread.Interrupt();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            //TODO: Re-implement
         }
     }
 }
