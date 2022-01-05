435f20bab902cfb3bfc66027f771219d5be08d27
oddbear <oddbjorn.bakke@gmail.com>
2022-01-05 21:50:27 +0100

    Added posibilty to set default device (missing in NAudio API).

---
+++ GoXLR.Force.Defaults/NotificationClient.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using GoXLR.Force.Defaults.Com;
+using NAudio.CoreAudioApi;
+using NAudio.CoreAudioApi.Interfaces;
+
+namespace GoXLR.Force.Defaults
+{
+    public class NotificationClient : IMMNotificationClient
+    {
+        private readonly Dictionary<string, MMDevice> _devices = new Dictionary<string, MMDevice>();
+        private readonly MMDeviceEnumerator _deviceEnumerator;
+        private readonly PolicyConfigClient _policyConfigClient;
+        
+        public NotificationClient()
+        {
+            _policyConfigClient = new PolicyConfigClient();
+            _deviceEnumerator = new MMDeviceEnumerator();
+            _deviceEnumerator.RegisterEndpointNotificationCallback(this);
+            
+            //Fill up device list (once):
+            var devices = _deviceEnumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.All);
+            foreach (var device in devices)
+            {
+                _devices[device.ID] = device;
+            }
+
+            //Set initial defaults to GoXLR defaults:
+            InitDefault(DataFlow.Render, Role.Multimedia, "System");
+            InitDefault(DataFlow.Render, Role.Communications, "Chat");
+            InitDefault(DataFlow.Capture, Role.Multimedia, "Chat Mic");
+            InitDefault(DataFlow.Capture, Role.Communications, "Chat Mic");
+
+            //Subscribe to mute/unmute and volume change events:
+            foreach (var mmDevice in GetGoXLRDevices())
+            {
+                OnDeviceAdded(mmDevice);
+            }
+        }
+
+        private void InitDefault(DataFlow flow, Role role, string searchString)
+        {
+            var defaultDevice = _deviceEnumerator.GetDefaultAudioEndpoint(flow, role);
+            SetDefaultAudioDevice(flow, role, defaultDevice.ID, searchString);
+        }
+
+        private MMDevice[] GetGoXLRDevices()
+        {
+            //TODO: What if it's not connected yet? Need to subscribe when connected and disconnected.
+            return _devices
+                .Values
+                .Where(device => device.DeviceFriendlyName.IndexOf("TC-Helicon GoXLR", StringComparison.OrdinalIgnoreCase) >= 0)
+                .Where(device => device.State == DeviceState.Active)
+                .ToArray();
+        }
+
+        private void SetDefaultDevice(string deviceId, Role role)
+        {
+            _policyConfigClient.SetDefaultEndpoint(deviceId, role);
+        }
+
+        private MMDevice GetActiveDevice(DataFlow flow, string searchString)
+        {
+            if (string.IsNullOrWhiteSpace(searchString))
+                return null;
+
+            return _devices
+                .Values
+                .Where(device => device.State == DeviceState.Active)
+                .Where(device => device.DataFlow == flow)
+                .Where(device => device.FriendlyName.StartsWith(searchString))
+                .SingleOrDefault();
+        }
+
+        private void SetDefaultAudioDevice(DataFlow flow, Role role, string pwstrDefaultDeviceId, string searchString)
+        {
+            if (string.IsNullOrWhiteSpace(pwstrDefaultDeviceId))
+                return;
+
+            var mmDevice = GetActiveDevice(flow, searchString);
+
+            if (mmDevice == null)
+                return;
+
+            if (pwstrDefaultDeviceId == mmDevice.ID)
+                return;
+
+            SetDefaultDevice(mmDevice.ID, role);
+        }
+
+        private void OnDeviceAdded(MMDevice device)
+        {
+            //TODO: Set as default if not already set.
+
+            //Add unmute and 100% volume fix.
+            device.AudioEndpointVolume.OnVolumeNotification += data =>
+            {
+                if (data.Muted)
+                    device.AudioEndpointVolume.Mute = false;
+
+                if (data.MasterVolume < 1)
+                    device.AudioEndpointVolume.MasterVolumeLevelScalar = 1;
+
+                Console.WriteLine($"{data.Guid} {data.Muted} {data.MasterVolume}");
+            };
+        }
+        
+        void IMMNotificationClient.OnDefaultDeviceChanged(DataFlow flow, Role role, string defaultDeviceId)
+        {
+            if (flow == DataFlow.Render && role == Role.Multimedia)
+            {
+                SetDefaultAudioDevice(flow, role, defaultDeviceId, "System");
+                return;
+            }
+
+            if (flow == DataFlow.Render && role == Role.Communications)
+            {
+                SetDefaultAudioDevice(flow, role, defaultDeviceId, "Chat");
+                return;
+            }
+
+            if (flow == DataFlow.Capture && role == Role.Multimedia)
+            {
+                SetDefaultAudioDevice(flow, role, defaultDeviceId, "Chat Mic");
+                return;
+            }
+
+            if (flow == DataFlow.Capture && role == Role.Communications)
+            {
+                SetDefaultAudioDevice(flow, role, defaultDeviceId, "Chat Mic");
+                return;
+            }
+
+            Console.WriteLine($"OnDefaultDeviceChanged: {flow}, {role}, {defaultDeviceId}");
+        }
+
+        void IMMNotificationClient.OnDeviceAdded(string pwstrDeviceId)
+        {
+            Console.WriteLine($"OnDeviceAdded: {pwstrDeviceId}");
+
+            var device = _deviceEnumerator.GetDevice(pwstrDeviceId);
+            if(device.DeviceFriendlyName.IndexOf("TC-Helicon GoXLR", StringComparison.OrdinalIgnoreCase) < 0)
+                return;
+
+            OnDeviceAdded(device);
+        }
+
+        void IMMNotificationClient.OnDeviceRemoved(string deviceId)
+        {
+            //TODO: Implement:
+            Console.WriteLine($"OnDeviceRemoved: {deviceId}");
+        }
+
+        void IMMNotificationClient.OnDeviceStateChanged(string deviceId, DeviceState newState)
+        {
+            Console.WriteLine($"OnDeviceStateChanged: {deviceId}, {newState}");
+        }
+
+        void IMMNotificationClient.OnPropertyValueChanged(string pwstrDeviceId, PropertyKey key)
+        {
+            Console.WriteLine($"OnPropertyValueChanged: {pwstrDeviceId}, {key.formatId} ... {key.propertyId}");
+        }
+    }
+}
--- GoXLR.Force.Defaults/WorkerService.cs
+++ GoXLR.Force.Defaults/WorkerService.cs
@@ -1,19 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using System.ServiceProcess;
 using System.Threading;
-using NAudio.CoreAudioApi;
 
 namespace GoXLR.Force.Defaults
 {
     public class WorkerService : ServiceBase
     {
-        private readonly Thread _workerThread = null;
         private readonly ManualResetEvent _manualResetEvent = new ManualResetEvent(false);
+        private NotificationClient _notificationClient;
 
         public WorkerService()
         {
@@ -24,120 +21,9 @@ namespace GoXLR.Force.Defaults
             base.CanPauseAndContinue = true;
             base.AutoLog = true;
 
-            _workerThread = new Thread(WorkerMethod)
-            {
-                Name = $"{name} Worker Thread",
-                IsBackground = true
-            };
-            _workerThread.Start();
+            _notificationClient = new NotificationClient();
         }
-
-        private void WorkerMethod()
-        {
-            while (true)
-            {
-                try
-                {
-                    _manualResetEvent.WaitOne();
-
-                    using (var enumerator = new MMDeviceEnumerator())
-                    {
-                        var defaultRenderMultimedia = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-                        var defaultRenderCommunications = enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Communications);
-                        var defaultCaptureMultimedia = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Multimedia);
-                        var defaultCaptureCommunications = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Communications);
-
-                        var audioDevices = enumerator.EnumerateAudioEndPoints(DataFlow.All, DeviceState.All);
-
-                        foreach (var device in audioDevices)
-                        {
-                            var deviceName = device.DeviceFriendlyName;
-                            if (deviceName.IndexOf("TC-Helicon GoXLR", StringComparison.OrdinalIgnoreCase) < 0)
-                            {
-                                device.Dispose();
-                                continue;
-                            }
-
-                            //Un-mute if muted:
-                            if (device.AudioEndpointVolume.Mute)
-                            {
-                                device.AudioEndpointVolume.Mute = false;
-                            }
-
-                            //Set Volume to 100% if lower:
-                            if (device.AudioEndpointVolume.MasterVolumeLevelScalar < 1)
-                            {
-                                device.AudioEndpointVolume.MasterVolumeLevelScalar = 1;
-                            }
-
-                            if (device.FriendlyName.StartsWith("Chat Mic"))
-                            {
-                                if (device == defaultCaptureMultimedia)
-                                {
-                                    //TODO: Any implementation of this in NAudio, or do I need something custom?
-                                    //AudioDeviceHelper.SetDefaultDeviceForRole(audioDevice, ERole.eMultimedia);
-                                    Console.WriteLine("Chat is not default capture multimedia");
-                                }
-
-                                if (device == defaultCaptureCommunications)
-                                {
-                                    //TODO: Any implementation of this in NAudio, or do I need something custom?
-                                    //AudioDeviceHelper.SetDefaultDeviceForRole(audioDevice, ERole.eCommunications);
-                                    Console.WriteLine("Chat is not default capture communication");
-                                }
-                            }
-
-                            if (device.FriendlyName.StartsWith("System"))
-                            {
-                                if (device == defaultRenderMultimedia)
-                                {
-                                    //TODO: Any implementation of this in NAudio, or do I need something custom?
-                                    //AudioDeviceHelper.SetDefaultDeviceForRole(audioDevice, ERole.eMultimedia);
-                                    Console.WriteLine("Chat is not default render multimedia");
-                                }
-                            }
-
-                            if (device.FriendlyName.StartsWith("Chat"))
-                            {
-                                if (device == defaultRenderCommunications)
-                                {
-                                    //TODO: Any implementation of this in NAudio, or do I need something custom?
-                                    //AudioDeviceHelper.SetDefaultDeviceForRole(audioDevice, ERole.eCommunications);
-                                    Console.WriteLine("Chat is not default render communication");
-                                }
-                            }
-
-                            device.Dispose();
-                        }
-
-                        defaultRenderMultimedia.Dispose();
-                        defaultRenderCommunications.Dispose();
-                        defaultCaptureMultimedia.Dispose();
-                        defaultCaptureCommunications.Dispose();
-                        
-                        //Not Working :(
-                        GC.Collect();
-                        GC.WaitForPendingFinalizers();
-                        GC.Collect();
-                        GC.WaitForPendingFinalizers();
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-
-                try
-                {
-                    Thread.Sleep(5);
-                }
-                catch (ThreadInterruptedException)
-                {
-                    //Thread was interupted (service stoped).
-                }
-            }
-        }
-
+        
 #if DEBUG
         public void Start()
         {
@@ -147,51 +33,22 @@ namespace GoXLR.Force.Defaults
 
         protected override void OnStart(string[] args)
         {
-            try
-            {
-                _manualResetEvent.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            //TODO: Re-implement
         }
 
         protected override void OnContinue()
         {
-            try
-            {
-                _manualResetEvent.Set();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            //TODO: Re-implement
         }
 
         protected override void OnPause()
         {
-            try
-            {
-                _manualResetEvent.Reset();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            //TODO: Re-implement
         }
 
         protected override void OnStop()
         {
-            try
-            {
-                _manualResetEvent.Reset();
-                _workerThread.Interrupt();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
+            //TODO: Re-implement
         }
     }
 }
